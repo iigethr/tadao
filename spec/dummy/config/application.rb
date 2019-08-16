cd12ab0777e42bb7b880a46f74ba9c704580a1d5
Tony de Faria <tonydefaria@iigethr.com>
2019-08-16 18:57:43 +0100

    removed build error

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -19,12 +19,19 @@ module Dummy
       end
     end
 
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Set the default locale
+    config.i18n.default_locale = :en
+
+    # Timezone configuration
+    config.time_zone = "London"
+    config.active_record.default_timezone = :local
+
+    # Remove default field errors
+    config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
   end
 end
